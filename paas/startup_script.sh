263d3b08a13b5bb8a7b11c29c751ddcc42d3b55f
Reza Rahman <reza_rahman@lycos.com>
2019-04-12 22:03:38 -0400

    Cleanup.

--- paas/startup_script.sh
+++
@@ -1,3 +0,0 @@
-# This file is executed when your app is started. In the portal, set the "Startup Script" field 
-# as the location of this file on the App Service instance (/home/site/deployments/tools/startup_script.sh).
-/opt/jboss/wildfly/bin/jboss-cli.sh -c --file=/home/site/deployments/tools/jboss_cli_commands.cli
