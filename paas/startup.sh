566bddda0f71f4672a9f3044ab5dda91d4456272
Reza Rahman <reza_rahman@lycos.com>
2022-12-28 20:59:15 -0500

    Fix path

--- paas/startup.sh
+++ paas/startup.sh
@@ -1,2 +1,2 @@
 # This file is executed when your app is started.
-$JBOSS_HOME/bin/jboss-cli.sh --connect --file=/site/deployments/tools/jboss_cli_commands.cli
+$JBOSS_HOME/bin/jboss-cli.sh --connect --file=/home/site/deployments/tools/jboss_cli_commands.cli
