5ef4da575400eac34cc41ddb93ab5abe60f708a3
Reza Rahman <reza_rahman@lycos.com>
2022-12-28 20:21:14 -0500

    Update FTP path.

--- paas/startup.sh
+++ paas/startup.sh
@@ -1,2 +1,2 @@
-# This file is executed when your app is started. FTP this file to /home.
-/opt/jboss/wildfly/bin/jboss-cli.sh -c --file=/home/site/deployments/tools/jboss_cli_commands.cli
+# This file is executed when your app is started.
+$JBOSS_HOME/bin/jboss-cli.sh --connect --file=/site/deployments/tools/jboss_cli_commands.cli
