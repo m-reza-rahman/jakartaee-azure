135a16e02daaf23c2946c9d96dcb19dcaa02e797
Philip Hayes <philip@deewhy.ie>
2023-04-18 15:33:17 -0400

    Moved to env variables for connection and JBoss BOM for dependencies

--- paas/jakartaee-cafe/src/main/scripts/startup.sh
+++ paas/jakartaee-cafe/src/main/scripts/startup.sh
@@ -1,2 +1,10 @@
 # This file is executed when your app is started.
-$JBOSS_HOME/bin/jboss-cli.sh --connect --file=/home/site/scripts/jboss_cli_commands.cli
+echo "Configuring JBoss"
+$JBOSS_HOME/bin/jboss-cli.sh --connect <<EOF
+module add --name=org.postgresql --resources=/home/site/libs/postgresql-42.5.3.jar --module-xml=/home/site/scripts/postgresql-module.xml
+/subsystem=datasources/jdbc-driver=postgresql:add(driver-name="postgresql",driver-module-name="org.postgresql",driver-class-name="org.postgresql.Driver",driver-xa-datasource-class-name="org.postgresql.xa.PGXADataSource")
+data-source add --name="PostgresqlDS" --driver-name="postgresql" --jndi-name="java:/jdbc/JakartaEECafeDB" --connection-url="${POSTGRESQLCONNSTR_CONNECTION_URL}" --user-name="${POSTGRESQLCONNSTR_USERNAME}" --password="${POSTGRESQLCONNSTR_PASSWORD}" --enabled=true --use-java-context=true
+reload --use-current-server-config=true
+exit
+EOF
+
