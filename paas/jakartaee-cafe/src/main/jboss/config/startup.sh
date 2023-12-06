d968246c3ce829c8894c63506970bc3f754e4a45
Reza Rahman <reza_rahman@lycos.com>
2023-12-06 16:45:54 -0500

    Update startup.sh

--- paas/jakartaee-cafe/src/main/jboss/config/startup.sh
+++ paas/jakartaee-cafe/src/main/jboss/config/startup.sh
@@ -1,2 +1,5 @@
+# Workaround for bug
+apt update && apt install unzip
+
 # This file is executed when your app is started.
 $JBOSS_HOME/bin/jboss-cli.sh --connect --file=/home/site/scripts/jboss_cli_commands.cli
